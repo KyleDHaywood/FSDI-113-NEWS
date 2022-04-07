c14dbb8df8b0e5b62466d517a3069b5b1a69c796
Kyle D Haywood <kylehaywood@hotmail.com>
2022-04-07 13:07:47 -0400

    Swapping WSL env

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, DeleteView
+from .models import Article
+
+
+
 
 # Create your views here.
+class ArticleCreateView(CreateView):
+    template_name="articles/create.html"
+    model=Article
+    fields= ['title', 'body']
+
+def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class ArticleDeleteView(DeleteView):
+    template_name="articles/delete"
--- config/settings.py
+++ config/settings.py
@@ -13,7 +13,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 from environs import Env
 
-env = Env.read_env()
+env = Env()
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,3 +132,8 @@ AUTH_USER_MODEL='accounts.CustomUser'
 
 # EMAIL
 EMAIL_BACKEND = ('django.core.mail.backends.%s.EmailBackend' % env.str("EMAIL_BACKEND", default='smtp'))
+
+EMAIL_PORT=587
+EMAIL_USE_TLS=True
+EMAIL_HOST=env.str("EMAIL_HOST")
+EMAIL_HOST_USER=env.str("EMAIL_HOST_USER")
